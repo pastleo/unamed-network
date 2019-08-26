27fd5a087d60961d9c6f52f05ba9197c83949062
PastLeo <chgu82837@gmail.com>
2019-08-27 02:31:10 +0800

    improve deploy script

--- example/deploy-site.sh
+++ example/deploy-site.sh
@@ -8,8 +8,9 @@ if [ -z "$TARGET" ]; then
   exit 1
 fi
 if [ -a "$TARGET" ]; then
-  echo "$TARGET exists"
-  exit 1
+  ARCHIVED_PATH="$BASEDIR/archived/$(date +%g%m%d%H%M)"
+  echo "$TARGET exists, move to $ARCHIVED_PATH"
+  mv "$TARGET" "$ARCHIVED_PATH"
 fi
 
 cp -rL "$BASEDIR" "$TARGET"
@@ -18,4 +19,5 @@ version=$(date +%g%m%d%H%M)
 mv "$TARGET/app" "$TARGET/app-$version"
 rm "$TARGET/deploy-site.sh"
 rm "$TARGET/prepare.sh"
-sed 's/src=".\/app\/browser.js"/src=".\/app-'$version'\/browser.js"/' "$BASEDIR/index.html" > "$TARGET/index.html"
+rm -r "$TARGET/archived"
+sed 's/.\/app\//.\/app-'$version'\//g' "$BASEDIR/index.html" > "$TARGET/index.html"
